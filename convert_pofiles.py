ec2c4021dfa0b5a48eb0cccfff17e76b6dcb4685
Christopher Allan Webber <cwebber@dustycloud.org>
2011-10-04 12:13:00 -0500

    Added convert_pofiles.py and updated licensing header in convert_templates_transtag.py

---
+++ convert_pofiles.py
@@ -0,0 +1,34 @@
+# pofile template syntax converter
+# 
+# Written in 2011 by Christopher Allan Webber, Creative Commons
+# 
+# To the extent possible under law, the author(s) have dedicated all
+# copyright and related and neighboring rights to this software to the
+# public domain worldwide. This software is distributed without any
+# warranty.
+# 
+# You should have received a copy of the CC0 Public Domain Dedication along
+# with this software. If not, see
+# <http://creativecommons.org/publicdomain/zero/1.0/>.
+
+# Convert pofiles from ${foo} style string substitutions to %(foo)s
+# style for the great Jinja2 {% trans %} block conversion ;)
+
+import re
+import sys
+
+def convert_pofile(filename):
+    podata = file(filename, 'r').read()
+    converted = re.sub(
+        "\$\{?(.+?)\}", lambda x: "%(" + x.groups()[0] + ")s", podata)
+    file(filename, 'w').write(converted)
+
+
+def main():
+    filenames = sys.argv[1:]
+
+    for filename in filenames:
+        convert_pofile(filename)
+
+if __name__ == '__main__':
+    main()
